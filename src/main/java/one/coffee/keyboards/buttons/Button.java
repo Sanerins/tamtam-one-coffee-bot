8ef26994d71c93c5447373c049bf08d8819cae2d
Artur Galeev <artur.galeev@vk.team>
2023-05-11 00:15:05 +0300

    add intent in Button.java

--- src/main/java/one/coffee/keyboards/buttons/Button.java
+++ src/main/java/one/coffee/keyboards/buttons/Button.java
@@ -1,24 +1,29 @@
 package one.coffee.keyboards.buttons;
 
 import chat.tamtam.botapi.model.CallbackButton;
+import chat.tamtam.botapi.model.Intent;
 
 public abstract class Button {
-    public String getPrefix() {
-        return this.getClass().getSimpleName();
-    }
-
     public final String keyboardPrefix;
     public final String text;
-
+    protected Intent intent;
     public Button(String keyboardPrefix, String text) {
         this.keyboardPrefix = keyboardPrefix;
         this.text = text;
+    }
+
+    public String getPrefix() {
+        return this.getClass().getSimpleName();
     }
 
     /**
      * @return готовую кнопку, в качестве payload для {@link chat.tamtam.botapi.model.CallbackButton} мы передаем keyboardPrefix.buttonPrefix(.доп данные(это не обязательно))
      */
     public chat.tamtam.botapi.model.Button build() {
-        return new CallbackButton(keyboardPrefix + "." + getPrefix(), text);
+        return new CallbackButton(keyboardPrefix + "." + getPrefix(), text).intent(intent);
+    }
+
+    public void setIntent(Intent intent) {
+        this.intent = intent;
     }
 }
