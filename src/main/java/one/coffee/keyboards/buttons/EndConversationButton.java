0b7fddd732a7589ec5f4f8fcf5a689d990ee9cdd
Sanerins <askosnitskiy@gmail.com>
2023-06-03 02:38:33 +0300

    Add intents

--- src/main/java/one/coffee/keyboards/buttons/EndConversationButton.java
+++ src/main/java/one/coffee/keyboards/buttons/EndConversationButton.java
@@ -1,7 +1,10 @@
 package one.coffee.keyboards.buttons;
 
+import chat.tamtam.botapi.model.Intent;
+
 public class EndConversationButton extends Button {
     public EndConversationButton(String keyboardPrefix) {
         super(keyboardPrefix, "Покинуть переписку");
+        intent = Intent.NEGATIVE;
     }
 }
--- src/main/java/one/coffee/keyboards/buttons/StartButton.java
+++ src/main/java/one/coffee/keyboards/buttons/StartButton.java
@@ -1,7 +1,10 @@
 package one.coffee.keyboards.buttons;
 
+import chat.tamtam.botapi.model.Intent;
+
 public class StartButton extends Button {
     public StartButton(String keyboardPrefix) {
         super(keyboardPrefix, "Начать диалог");
+        intent = Intent.POSITIVE;
     }
 }
